94554a6c0e40634a8755a92245e226835367a015
Luis Fernando <raintech@outlook.com>
2022-09-25 19:05:21 -0300

    adicionado controller Alunos

---
+++ Controllers/AlunosController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebServiceRestfull.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebServiceRestfull.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunosController : ControllerBase
+    {
+        // GET: api/<AlunosController>
+        [HttpGet]
+        public IEnumerable<Aluno> Get()
+        {
+            List<Aluno> alunos = new List<Aluno>
+            {
+                new Aluno("Luana", 25),
+                new Aluno("João", 41)
+            };
+
+            alunos.Add(new Aluno("Luana", 25));
+
+            return alunos;
+        }
+
+        // GET api/<AlunosController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+
+
+            return "value";
+        }
+
+        // POST api/<AlunosController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AlunosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AlunosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Entities/Aluno.cs
@@ -0,0 +1,14 @@
+﻿namespace WebServiceRestfull.Entities
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Aluno(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+    }
+}
