cc43e3618a44c741122b9341a2b64327bb3c6971
Luis Fernando <raintech@outlook.com>
2022-09-25 19:54:38 -0300

    Ajustado construtor de Aluno

--- Entities/Aluno.cs
+++ Entities/Aluno.cs
@@ -1,23 +1,24 @@
 ﻿using System.Reflection.PortableExecutable;
+using WebServiceRestfull.Enums;
 
 namespace WebServiceRestfull.Entities
 {
     public class Aluno
     {
-        public int IdAluno { get; set; } 
-        public string NomeAluno { get; set; }
-        public int IdadeAluno { get; set; } 
-        public double AlturaAluno { get; set; } 
-        public double PesoAluno { get; set; }   
-        public string GeneroAluno { get; set; }
+        public int Id { get; set; } 
+        public string Nome { get; set; }
+        public int Idade { get; set; } 
+        public double Altura { get; set; } 
+        public double Peso { get; set; }   
+        public Genero Genero { get; set; }
         
-        public void CadastrarAluno(string nome, int idade, double altura, double peso,string genero )
+        public Aluno(string nome, int idade, double altura, double peso, Genero genero)
         {
-        this.NomeAluno = nome;
-        this.IdadeAluno = idade;
-        this.AlturaAluno = altura;
-        this.PesoAluno = peso;
-        this.GeneroAluno = genero;
+            this.Nome = nome;
+            this.Idade = idade;
+            this.Altura = altura;
+            this.Peso = peso;
+            this.Genero = genero;
         }
     }
 }
